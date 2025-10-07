5dac304c519a7d8314e50ae29bd6f6bdf92331ee
pratchjp <142291046+Pratchaya-jamp@users.noreply.github.com>
2025-10-07 18:36:13 +0700

    update db connection

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 // ตั้งค่าการเชื่อมต่อฐานข้อมูล
 export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root', // เปลี่ยนเป็น username ของคุณ
-    password: '@june3009', // เปลี่ยนเป็น password ของคุณ
+    host: 'mysql-39fc22c0-sxu-dcc5.b.aivencloud.com',
+    user: 'avnadmin', // เปลี่ยนเป็น username ของคุณ
+    password: 'AVNS_3tl0juXutq9_ihSG2s8', // เปลี่ยนเป็น password ของคุณ
     database: 'itb',
     waitForConnections: true,
     connectionLimit: 10,
