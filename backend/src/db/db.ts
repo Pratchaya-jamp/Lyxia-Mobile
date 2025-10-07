98823ffeaa669dcbef614bbf218802c781812f49
pratchjp <142291046+Pratchaya-jamp@users.noreply.github.com>
2025-10-07 18:43:49 +0700

    update db port

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -6,9 +6,13 @@ export const pool = mysql.createPool({
     user: 'avnadmin', // เปลี่ยนเป็น username ของคุณ
     password: 'AVNS_3tl0juXutq9_ihSG2s8', // เปลี่ยนเป็น password ของคุณ
     database: 'itb',
+    port: 26374, 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 console.log("Database pool created and ready to connect.");
